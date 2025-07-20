0c2bcd66a4afbce20b0afbf55c3f9a148398d74a
KcVerze <kcverze@gmail.com>
2025-07-20 13:21:04 +0100

    1

---
+++ 1.py
@@ -0,0 +1,36 @@
+# python practice
+
+def say_hello():
+    print("hey")
+
+say_hello()
+
+print("hello world")
+
+#basic operation
+
+num1= input("enter a number: ")
+num2= input("enter another: ")
+
+sum = int(num1) + int(num2)
+print("the sum is:", sum)
+
+# decision making
+
+age = int (input("How old are you:"))
+
+if age >= 18:
+    print("you're an adult. ")
+else:
+    print("small boy")
+
+
+score = int( input("Student math score: "))
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+else:
+    print("Grade: F")
