addc74f842c4f7b48bbc263ee53456be8fee2ddf
KcVerze <kcverze@gmail.com>
2025-07-21 22:20:12 +0100

    6

--- 1.py
+++ 1.py
@@ -72,16 +72,74 @@ else:
 
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
-    print("You can ride the rollercoaster")
+    print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
-    if age <= 12:
-        print("Please pay $5.")
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
+
 else:
-    print("Sorry you have to grow taller before you can ride.")
+    print("Sorry, you have to grow taller before you can ride.")
+
+
+
+# Treasure island
+
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+decision1 = input("Direction -- left or right ?")
+if decision1 == "left":
+    decision2 = input("Decision -- Swim or Wait")
+    if decision2 == "wait":
+        decision3 = input("Which door?")
+        if decision3 == "blue":
+            print("Eaten by beasts. Game Over")
+        elif decision3 == "yellow":
+            print("You win")
+        elif decision3 == "red":
+            print("burned by fire. Game Over")
+        else:
+            print ("Game Over")
+    else:
+        print("Attacked by trout Game Over.")
+else:
+    print("Fall into a hole Game Over.")
 
