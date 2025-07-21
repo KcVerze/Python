0069ab614b80ba1f047a5f5711fd3168495c791a
KcVerze <kcverze@gmail.com>
2025-07-21 20:17:58 +0100

    3

--- 1.py
+++ 1.py
@@ -48,3 +48,15 @@ city = input("What's the name of the city you grew up in? ")
 
 pet_name = input("What's your pet name? ")
 print("your band name could be : "+city+" "+pet_name)
+
+
+# tip calculator
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+final_bill = (bill+(bill*(tip/100)))/people
+bill_roundup = (round(final_bill,2))
+print(f"${bill_roundup}")
+
