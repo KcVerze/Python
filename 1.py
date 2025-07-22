f29f88e891db52a092211b5ecc5415e0e9036e51
KcVerze <kcverze@gmail.com>
2025-07-22 12:33:47 +0100

    9

--- 1.py
+++ 1.py
@@ -216,3 +216,24 @@ elif sel1 > sel2:
     print("You win!")
 elif sel2 == sel1:
     print("It's a draw!")
+
+
+
+# exam score
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+
+total_exam_score = sum(student_scores)
+
+total = 0
+for score in student_scores:
+    total += score
+
+# Max score
+max_score = 0
+
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+
+print(max_score)
+
