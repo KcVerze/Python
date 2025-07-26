c427b848803a90b3f28d7d88a56cd3c9663bdb87
KcVerze <kcverze@gmail.com>
2025-07-26 08:51:14 +0100

    13

--- 1.py
+++ 1.py
@@ -358,3 +358,19 @@ def game_load():
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
 #  is, "Wrong" if it's not.
+
+# hangman v-1 :
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+guess = input("Guess a Letter: ").lower()
+
+if guess not in chosen_word:
+    print("Right")
+else:
+    print("wrong")
+
