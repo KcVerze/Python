f7ac374fbcd40f6945c92fc890e078eed4a02f44
KcVerze <kcverze@gmail.com>
2025-07-26 20:50:07 +0100

    18

--- 1.py
+++ 1.py
@@ -641,3 +641,14 @@ def greet():
     print("hello")
 
 greet()
+
+
+# life in weeks :
+
+def life_in_weeks(age):
+    years_left = 90 - int(age)
+    weeks_left = int(years_left) * 52
+    print(f"You have {weeks_left} weeks left." )
+    
+
+life_in_weeks(56)
