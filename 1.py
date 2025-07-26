fd62c3f232a359a71ce2db00db71fd1277acb6df
KcVerze <kcverze@gmail.com>
2025-07-26 09:22:05 +0100

    14

--- 1.py
+++ 1.py
@@ -369,8 +369,49 @@ print(chosen_word)
 
 guess = input("Guess a Letter: ").lower()
 
-if guess not in chosen_word:
-    print("Right")
-else:
-    print("wrong")
+for letter  in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("wrong")
+
+
+# Hang man v-2 
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
+placeholder = ""
+word_length = len(chosen_word)
+for letter in range(chosen_word):
+    placeholder += "_"
+
+print(placeholder)
+
+guess = input("Guess a letter: ").lower()
+
+# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+
+display = ""
+
+for letter in chosen_word:
+    if letter == guess:
+        print(guess)
+        display += guess
+    else:
+        print("_")
+        display += "_"
+print(display)
+
+
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
 
